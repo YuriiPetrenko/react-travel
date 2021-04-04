05bc2a0291cf0b8a8d1f75c1d0dc79ffb7f22dbd
yuriipetrenko <obzora6@gmail.com>
2021-04-04 17:40:09 +0300

    add Card component

--- src/components/pages/Home.js
+++ src/components/pages/Home.js
@@ -1,10 +1,13 @@
 import '../../App.css'
+import Cards from '../Cards';
 import HeroSection from '../HeroSection'
+
 
 function Home(){
     return (
         <>
             <HeroSection/>
+            <Cards/>
         </>
     )
 }
